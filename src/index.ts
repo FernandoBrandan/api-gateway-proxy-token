46722840ec478b22efa2cf3d16d16aa275726bc5
fernandobrandan <fernando.brandan1@hotmail.com>
2025-02-26 18:29:33 -0300

    del vercel-npm

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
 import "./config/database";
-//let port = process.env.PORT || 3001;
+let port = process.env.PORT || 3001;
 
-import { VercelRequest, VercelResponse } from "@vercel/node";
-// Adaptar Express a serverless:
-export default (req: VercelRequest, res: VercelResponse) => {
-    return app(req, res);
-};
-
-//app.listen(port, () => {
-//    console.log(`apigateway - Server listen on port http://localhost:${port}`);
-//});
+app.listen(port, () => {
+    console.log(`apigateway - Server listen on port http://localhost:${port}`);
+});
 
