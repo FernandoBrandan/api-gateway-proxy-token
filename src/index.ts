3496cfae0205f24e65957fc342194449c0f4a366
fernandobrandan <fernando.brandan1@hotmail.com>
2025-02-26 18:12:37 -0300

    deploy vercel

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import app from "./app";
 import "./config/database";
 let port = process.env.PORT || 3001;
 
+import { VercelRequest, VercelResponse } from "@vercel/node";
+// Adaptar Express a serverless:
+export default (req: VercelRequest, res: VercelResponse) => {
+    return app(req, res);
+};
+
 app.listen(port, () => {
     console.log(`apigateway - Server listen on port http://localhost:${port}`);
 });
